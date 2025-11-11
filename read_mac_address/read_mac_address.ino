dac41c98cc977c4bb854f0ed5830d16cb73b44fc
DemiFiend01 <maggie.mr1606@gmail.com>
2025-11-11 14:42:08 +0100

    Added setMacAddress function

--- read_mac_address/read_mac_address.ino
+++ read_mac_address/read_mac_address.ino
@@ -6,7 +6,30 @@
 
 //if we need to set the MAC address
 //esp_err_t esp_wifi_set_mac(wifi_interface_t ifx, const uint8_t mac[6])
-//esp_err_t is the 
+
+//this function can ONLY be called before the interface is enabled.
+//The bit 0 of the first byte of MAC address can not be 1.
+void setMacAddress(uint8_t mac[8])
+{
+  esp_err_t ret = esp_wifi_set_mac(WIFI_IF_STA, mac); //station interface
+
+  if (ret == ESP_OK) { //succeeded
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  mac[0], mac[1], mac[2],
+                  mac[3], mac[4], mac[5]); //printing the MAC address to be copied into the sender's code
+  } else if (ret == ESP_ERR_WIFI_NOT_INIT){
+    Serial.println("WiFi is not initialized by esp_wifi_init.");
+  } else if (ret == ESP_ERR_INVALID_ARG){
+    Serial.println("Invalid argument.");
+  }else if (ret == ESP_ERR_WIFI_IF){
+    Serial.println("Invalid interface. Is not STATION.");
+  }else if(ret == ESP_ERR_WIFI_MAC){
+    Serial.println("Invalid mac address. Remember that the bit 0 of the first byte of MAC address can not be 1.")
+  }else if(ret == ESP_ERR_WIFI_MODE){
+    Serial.println("WiFi mode is wrong.")
+  }
+
+}
 
 void readMacAddress(){
 
@@ -18,9 +41,9 @@ void readMacAddress(){
                   baseMac[0], baseMac[1], baseMac[2],
                   baseMac[3], baseMac[4], baseMac[5]); //printing the MAC address to be copied into the sender's code
   } else if (ret == ESP_ERR_WIFI_NOT_INIT){
-    Serial.println("WiFi is not initialized by esp_wifi_init");
+    Serial.println("WiFi is not initialized by esp_wifi_init.");
   } else if (ret == ESP_ERR_INVALID_ARG){
-    Serial.println("Invalid argument");
+    Serial.println("Invalid argument.");
   }else if (ret == ESP_ERR_WIFI_IF){
     Serial.println("Invalid interface. Is not STATION.");
   }
@@ -32,6 +55,8 @@ void setup(){
   Serial.println("Reading the MAC address:");
 
   WiFi.mode(WIFI_STA); //Station mode (client)
+  //const uint8_t newMac[6] = {0x1a, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
+  //setMacAddress();
   WiFi.STA.begin(); //start the wi-fi
 
   Serial.print("[DEFAULT] ESP32 Board MAC Address: ");
